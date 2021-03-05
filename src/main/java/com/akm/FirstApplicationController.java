67d79050c89c39d0d4e85bd174b8dc1278fa1080
Apple <apple@Ashish.local>
2021-03-05 22:01:36 +0530

    Changed url of demo page

--- src/main/java/com/akm/FirstApplicationController.java
+++ src/main/java/com/akm/FirstApplicationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstApplicationController 
 {
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/hello")
 	   public String hello() {
 	      return "Welcome to my First Spring Boot Application ";
 	   }
--- src/main/java/com/akm/FirstSpringBootApplication.java
+++ src/main/java/com/akm/FirstSpringBootApplication.java
@@ -2,9 +2,10 @@ package com.akm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstSpringBootApplication {
+public class FirstSpringBootApplication  extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
