9aca8e93b7dafd30a27619ce88ffad108c745c91
Apple <apple@Ashish.local>
2021-03-05 22:19:28 +0530

    Test  spring beet Application Runner

--- src/main/java/com/akm/FirstSpringBootApplication.java
+++ src/main/java/com/akm/FirstSpringBootApplication.java
@@ -1,17 +1,23 @@
 package com.akm;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstSpringBootApplication  extends SpringBootServletInitializer  {
+public class FirstSpringBootApplication  implements ApplicationRunner  {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		SpringApplication.run(FirstSpringBootApplication.class,args);
 
+	}
+
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Testing out  Application Runner ");
+		
 	}
 
 }
